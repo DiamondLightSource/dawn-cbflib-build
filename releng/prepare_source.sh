236687dd9e09596b9c39ef20ba6aecdf5f3fa05f
Peter Chang <peter.chang@diamond.ac.uk>
2023-05-10 09:07:05 +0100

    Fix recursive HDF5CFLAGS and use symbolic link to releng

--- releng/prepare_source.sh
+++ releng/prepare_source.sh
@@ -1,7 +1,7 @@
 # 
 git clone --depth=1 --branch=CBFlib-0.9.7-devel https://github.com/yayahjb/cbflib.git
-mv releng cbflib
 cd cbflib
+ln -s ../releng .
 patch -p1 < releng/make.patch
 patch -p1 < releng/no-hdf5.patch
 patch -p1 < releng/pointer-leak.patch
