9aaad12d8265515e1f4f4e29706325ca40a02e73
Peter Chang <peter.chang@diamond.ac.uk>
2026-03-12 16:14:34 +0000

    Update to CBFlib 0.9.8
    Use tarball from DIALS, update Makefile.m4 patch, remove win32 cross
    compiling and add win32 GH workflow. Also update actions versions

--- releng/prepare_source.sh
+++ releng/prepare_source.sh
@@ -1,12 +1,24 @@
 # 
-if [ ! -d cbflib ]; then
-  git clone --depth=1 --branch=CBFlib-0.9.7-devel https://github.com/yayahjb/cbflib.git
+CBFLIB_VER=0.9.8
+CBFLIB_CHK=81d8ea4b75ba2cab7f94a6b20a32b8ca099405a073201776c64064b64c7a4d30
+CBFLIB_DIR=CBFlib-${CBFLIB_VER}
+CBFLIB_TGZ=${CBFLIB_DIR}.tar.gz
+
+if [ ! -f ${CBFLIB_TGZ} ]; then
+  curl -fsSLO "https://github.com/dials/cbflib/archive/refs/tags/${CBFLIB_TGZ}"
+  echo "${CBFLIB_CHK} ${CBFLIB_TGZ}" | sha256sum -c -
 fi
-cd cbflib
-if [ ! -L releng ]; then
+
+if [ ! -d "${CBFLIB_DIR}" ]; then
+  tar xzf "${CBFLIB_TGZ}"
+  mv cbflib-${CBFLIB_DIR} ${CBFLIB_DIR}
+  pushd "${CBFLIB_DIR}"
+
   ln -s ../releng .
   patch -p1 < releng/make.patch
   patch -p1 < releng/no-hdf5.patch
   patch -p1 < releng/pointer-leak.patch
+  popd
 fi
 
+cd "${CBFLIB_DIR}"
